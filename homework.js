c100874c14dab6dc0e1cb5f4cb480720628a1b77
kotsu1018 <125286535+kotsu1018@users.noreply.github.com>
2023-06-20 22:53:07 +0900

    homework

---
+++ homework.js
@@ -0,0 +1,14 @@
+let admin;
+let name;
+name = "John";
+admin = name;
+// コピーしてnameと同じものを箱の中に入れるイメージ
+console.log(admin);
+
+// letを使えば変数をいろんな値を入れることができる
+// constを使えば１つの値しか入れられない
+
+let nameFieldChecked = true;
+
+let isGreater = 4 > 1;
+console.log(isGreater);
