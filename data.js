9c762731917cf2d45ab7c8c198ed97d4917f8b0d
kotsu1018 <125286535+kotsu1018@users.noreply.github.com>
2023-06-21 23:17:54 +0900

    date.js

---
+++ data.js
@@ -0,0 +1,13 @@
+let isGreater = 4 > 1;
+console.log(isGreater);
+// この型は通常 yes/no の値を格納するために使われます:
+//  true は “yes” を意味し、 false は “no, 正しくない” を意味します。
+
+// null 値
+let age = null;
+// “無し”、“空” または “不明な値” と言った意味を持つ特別な値です。
+
+// undefined 値
+let x;
+console.log(x);
+// undefined の意味は “値は代入されていません” です。
--- homework.js
+++ homework.js
@@ -7,8 +7,3 @@ console.log(admin);
 
 // letを使えば変数をいろんな値を入れることができる
 // constを使えば１つの値しか入れられない
-
-let nameFieldChecked = true;
-
-let isGreater = 4 > 1;
-console.log(isGreater);
