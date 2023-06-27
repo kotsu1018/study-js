d25f043325dde5e5148be52bd0d591cb50cece12
kotsu1018 <125286535+kotsu1018@users.noreply.github.com>
2023-06-27 22:42:56 +0900

    arrow

--- homework/homework.js
+++ homework/homework.js
@@ -7,3 +7,20 @@ console.log(admin);
 
 // letを使えば変数をいろんな値を入れることができる
 // constを使えば１つの値しか入れられない
+
+let array = [1, 2, 3, 4, 5]; //lengthは配列の数を表してくれる
+let index = 0;
+
+while (index < array.length) {
+  console.log(array[index]);
+  index++;
+}
+
+let index = 0;
+let sum = 0;
+
+while (index < 100) {
+  index++;
+  sum += index;
+  console.log(sum);
+}
---
+++ test3.js
@@ -0,0 +1,21 @@
+let first = [1, 2];
+let second = [10, 2];
+let third = [5, 5];
+
+if (first[0] < first[1]) {
+  console.log("<");
+} else {
+  console.log(">");
+}
+
+if (second[0] < second[1]) {
+  console.log("<");
+} else {
+  console.log(">");
+}
+
+if (third[0] < third[1]) {
+  console.log("=");
+} else {
+  console.log("=");
+}
