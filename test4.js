f8ab7f6d7127e4c1b17cdf0cea5a8248ffd31bca
kotsu1018 <125286535+kotsu1018@users.noreply.github.com>
2023-07-11 22:44:48 +0900

    test4-2

--- test4.js
+++ test4.js
@@ -8,15 +8,39 @@ if (animals === "penguin") {
   alert("動物じゃないやんけ！");
 }
 
-console.log(typeof "kotone");
-// typeofは中のそのものを出力するのではなく値がどの種類のデータかを判定した結果を出力される
-// この場合はstringと出る（文字列）という意味
+// console.log(typeof "kotone");
+// // typeofは中のそのものを出力するのではなく値がどの種類のデータかを判定した結果を出力される
+// // この場合はstringと出る（文字列）という意味
 
-// 数値（nummber)
-// 文字列（string)
+// // 数値（nummber)
+// // 文字列（string)
 
-// 真偽値（boolean)
+// // 真偽値（boolean)
 console.log(typeof true); //Boolean
 console.log(typeof "true"); //string
 console.log(false);
 // これをtypeof使って出力させると(boolean)とでる
+
+// if文はtrueかfalseのどちらかで答えがでる
+// 例）
+const X = 3;
+if (X) {
+  alert("この文は正しいです");
+} else {
+  alert("この文は間違っています");
+}
+
+// この場合は数値が３だからfalseで認識されて上の文が実行される
+// 数値は「０」か「-0」じゃなかったらtrueになるのがjavascriptのルール
+
+let X = 1;
+if (X > 3) {
+  alert("3よりおおきい");
+} else if (X === 3) {
+  //else if　をいくつも分岐することができる
+  //等しいと書きたい場合は===イコールを３つ
+  alert("3と同じです");
+} else {
+  //上のどちらもfalseなら一番下が実行される
+  alert("3より小さいです");
+}
